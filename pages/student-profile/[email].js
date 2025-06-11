f903bdca5b4079d6c6c8b26b9fca6ec8dd03e378
BBSCash <ben-sheehan1@hotmail.com>
2025-06-11 20:53:08 +0100

    Update [email].js

--- pages/student-profile/[email].js
+++ pages/student-profile/[email].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import Header from '@/components/Header';
+import Header from '@/components/header.js';
 import Link from 'next/link';
 import withAuth from '@/components/withAuth';
 
