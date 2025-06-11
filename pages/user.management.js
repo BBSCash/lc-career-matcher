d7558483276ab9bb84821004929f4e67ca07f29d
BBSCash <ben-sheehan1@hotmail.com>
2025-06-11 13:49:42 +0100

    Update user.management.js

--- pages/user.management.js
+++ pages/user.management.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import withAuth from '@/components/withAuth';
-import Header from '@/components/Header';
+import Header from '@/components/header.js';
 
 function UserManagement({ user }) {
   const [users, setUsers] = useState([]);
